148b6f036cb6f94d7a218fc18fe5df70759d289a
yongjian.wan <yongjian.wan@ecovacs.com>
2017-07-24 15:27:43 +0800

    wanshan

---
+++ YJTangguo/YJTangguo/TableCell/YJHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  YJHeaderView.h
+//  YJTangguo
+//
+//  Created by wanyongjian on 2017/7/24.
+//  Copyright © 2017年 eco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YJHeaderView : UIView
+
+@end
---
+++ YJTangguo/YJTangguo/TableCell/YJHeaderView.m
@@ -0,0 +1,21 @@
+//
+//  YJHeaderView.m
+//  YJTangguo
+//
+//  Created by wanyongjian on 2017/7/24.
+//  Copyright © 2017年 eco. All rights reserved.
+//
+
+#import "YJHeaderView.h"
+
+@implementation YJHeaderView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
